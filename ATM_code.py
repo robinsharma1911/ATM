ffa167852d68040974bc38ec0eb3bd96a2519568
Robin Sharma <robinsharma1911@gmail.com>
2020-10-10 17:54:23 +0530

    Add files via upload

--- ATM_code.py
+++ ATM_code.py
@@ -1,52 +1,56 @@
-accounts = {822466:[1234,2000],822456:[2345,3000]}
 
-#check balance
-def check_balance(account_number, amt):
-    print('account number:-',account_number,'\ntotal balance:-',amt)
-    main_function(account_number, amt)
+#accounts dict contains:-
+    #keys as account no's of two persons and value[0] as pins and value[1] as balance
+accounts = {822466:[1234,20000],822456:[2345,30000]}
 
-#withdraw balance
-def withdraw(account_number, amt):
-    withdraw_amt = int(input('\nenter amount for withdraw:-'))
-    amt=amt-withdraw_amt
-    print('\nafter withdraw remaining balance:-',amt)
-    #update dict amt at run time
-    accounts[account_number][1] =amt
-    print(accounts)
-    main_function(account_number, amt)
 
 #main function after authentication
-def main_function(account_number, amt):
-    print(
-        '\nchoose option'
-        '\n 1)check balance'
-        '\n 2)withdraw'
-        '\n 3)exit\n'
-    )
-    select = int(input('choice:-'))
-    if select == 1:
-        check_balance(account_number, amt)
-    elif select == 2:
-        withdraw(account_number, amt)
-    elif select == 3:
-        exit()
-    else:
-        print('\nwrong choice try again')
-        main_function(account_number, amt)
+def main_function(account_number, accounts):
+    while True:
+        amt = accounts[account_number][1]           #save balance of current user in amt variable
+        #check balance
+        def check_balance(account_number, amt):
+            print('account number:-',account_number,'\ntotal balance:-',amt)
+        #withdraw balance
+        def withdraw(account_number, amt): 
+            withdraw_amt = int(input('\nenter amount for withdraw:-'))
+            amt=amt-withdraw_amt
+            print('\nafter withdraw remaining balance:-',amt)                                         
+            accounts[account_number][1] =amt                             #update dict balance at run time
+            print(accounts)
+         
+        print(                                                           #choose  options
+            '\nchoose option'
+            '\n 1)check balance'
+            '\n 2)withdraw'
+            '\n 3)exit\n'
+        )
+        select = int(input('choice:-'))
+        if select == 1:
+            check_balance(account_number, amt)
+        elif select == 2:
+            withdraw(account_number, amt)
+        elif select == 3:
+            exit()
+        else:
+            print('\nwrong choice try again')
+
+
 
 #function to authenticate user account number and pin..
 def authentication():
-    #___take input from user for authenticate account no and pin....
-    account_number = int(input('enter account number:-'))
+    account_number = int(input('enter account number:-'))                #___take input from user for account no. and pin....
     pin = int(input('enter pin:-'))
 
     for acc, pins in accounts.items():
         if account_number == acc:
             if pin == pins[0]:
                 amt = pins[1]
-                main_function(account_number, amt)
+                main_function(account_number,accounts)                    #call main_function after successful authenticate a person...
             else:
                 print('wrong credentials')
                 authentication()
 
+
+#program starts here...
 authentication()
